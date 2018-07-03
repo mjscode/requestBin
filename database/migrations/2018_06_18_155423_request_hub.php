c231117218f723aea38e1e6182848208fc773452
vagrant <vagrant@homestead>
2018-07-03 15:35:08 +0000

    something

--- database/migrations/2018_06_18_155423_request_hub.php
+++ database/migrations/2018_06_18_155423_request_hub.php
@@ -15,16 +15,16 @@ class RequestHub extends Migration
     {
         Schema::create('requestHubs', function (Blueprint $table) {
             $table->increments('id');
-           
+            $table->string('username');
             $table->string('url_id');
             $table->string('IP_Address');
             $table->json('headers');
             $table->string('method');
-            $table->string('url_content');
-            
+            $table->string('url_content');    
             $table->string('query_keys')->default('none');
             $table->string('query_values')->default('none');
             $table->json('request_body');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
